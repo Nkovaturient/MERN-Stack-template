dbd13b54894c8e0b45e1e99e1f48c467293e9288
unknown <nk8981398@gmail.com>
2024-08-26 20:46:41 +0530

    renamed directories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='text-3xl text-center text-red-700'
-    >Welcome to Vite with TailwindCSS and React </div>
+    >Welcome to MERN Stack Template </div>
   )
 }
 
